2a83fd1abd5ee6de39719cb4c9895e78bdda2ebf
noven temer <noven5823@gmail.com>
2018-07-18 19:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function prependDriver(name){
 	return [name,...drivers]
 }
 function removeLastDriver(){
-  return drivers[drivers.length - 1];
+ return drivers.slice(0, drivers.length - 1);
 }
 function removeFirstDriver () {
   return drivers.slice(1);
